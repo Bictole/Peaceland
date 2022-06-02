19356fdd4e90937d8a2bec70fda0501ca8a23b97
alexandre.lemonnier <alexandre.lemonnier@epita.fr>
2022-06-02 21:45:09 +0200

    feat(Main): generate multiple report with delay with multiple person

--- peacewatcher_simulator/src/main/scala/peacewatcher/Main.scala
+++ peacewatcher_simulator/src/main/scala/peacewatcher/Main.scala
@@ -7,31 +7,38 @@ import scala.io.Source
 
 object Main {
     
-    def generateData(names: JsArray, words: List[String]): Event = {
-        val r = scala.util.Random
-        val size = names.value.size
-        Event(
-            r.nextInt,
-            LocalDateTime.now(),
-            Coords(r.nextDouble, r.nextDouble),
-            List(words(r.nextInt(words.size))),
-            List(
-                Person(
-                    (names.value(r.nextInt(size)) \ "name").as[String],
-                    r.nextDouble,
-                )
-            )
-        )
-    }
-    
-    def main(args: Array[String]): Unit = {
+    def generateData(names: JsArray, words: List[String]) = {
         
         // Declare classes format to serialize
         implicit val personWrites = Json.format[Person]
         implicit val coordsWrites = Json.format[Coords]
         implicit val eventWrites = Json.format[Event]
 
-        // Import citizens'names
+        val size = names.value.size
+        (0 to 10).foreach((i) => {
+            val event = Event(
+                Random.nextInt,
+                LocalDateTime.now(),
+                Coords(Random.nextDouble, Random.nextDouble),
+                List(words(Random.nextInt(words.size))),
+                (0 to Random.nextInt(5)).map(p => 
+                        Person(
+                            (names.value(Random.nextInt(size)) \ "name").as[String],
+                            Random.nextDouble
+                        )).toList
+            )
+            val eventJsonString = Json.stringify(Json.toJson(event))
+            println(eventJsonString)
+            Thread.sleep(1000)
+        })
+
+        
+        
+    }
+    
+    def main(args: Array[String]): Unit = {
+        
+                // Import citizens'names
         val namesFileName = "src/main/resources/name.json"
         val fSource = Source.fromFile(namesFileName)
         val namesRaw = fSource.getLines.mkString
@@ -42,8 +49,7 @@ object Main {
         val phrases = List("Hello", "Goodbye", "Help me!", "Mayday", "Scala is a plusgood language", "I come in peace! Don't shoot!")
         
         // Generate events
-        val event = generateData(namesJson.as[JsArray], phrases)
-        val eventJsonString = Json.stringify(Json.toJson(event))
-        println(eventJsonString)
+        generateData(namesJson.as[JsArray], phrases)
+        
     }
 }
