da1bbd469b279e3c851e34f284b203ab860199ae
alexandre.lemonnier <alexandre.lemonnier@epita.fr>
2022-06-02 22:19:11 +0200

    feat(peacewatcher): add kafka stream producer

--- peacewatcher_simulator/src/main/scala/peacewatcher/Main.scala
+++ peacewatcher_simulator/src/main/scala/peacewatcher/Main.scala
@@ -1,13 +1,16 @@
 package peacewatcher
 
+import java.util.Properties
 import play.api.libs.json._
 import java.time.LocalDateTime
 import scala.util.Random
 import scala.io.Source
-
+import org.apache.kafka.clients.producer._
+import org.apache.kafka.common.serialization.StringSerializer
+        
 object Main {
     
-    def generateData(names: JsArray, words: List[String]) = {
+    def generateData(names: JsArray, words: List[String], producer: KafkaProducer[String, String]) = {
         
         // Declare classes format to serialize
         implicit val personWrites = Json.format[Person]
@@ -29,16 +32,24 @@ object Main {
             )
             val eventJsonString = Json.stringify(Json.toJson(event))
             println(eventJsonString)
+            val record = new ProducerRecord[String, String]("peaceland", "event", eventJsonString)
+            producer.send(record)
             Thread.sleep(1000)
         })
-
-        
-        
     }
     
     def main(args: Array[String]): Unit = {
         
-                // Import citizens'names
+        val server = if(args.size == 1) args(0) else "localhost"
+        val props = new Properties()
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, s"$server:9092")
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+
+
+        val producer : KafkaProducer[String, String] = new KafkaProducer[String, String](props)
+
+        // Import citizens'names
         val namesFileName = "src/main/resources/name.json"
         val fSource = Source.fromFile(namesFileName)
         val namesRaw = fSource.getLines.mkString
@@ -49,7 +60,9 @@ object Main {
         val phrases = List("Hello", "Goodbye", "Help me!", "Mayday", "Scala is a plusgood language", "I come in peace! Don't shoot!")
         
         // Generate events
-        generateData(namesJson.as[JsArray], phrases)
+        generateData(namesJson.as[JsArray], phrases, producer)
+
+        producer.close()
         
     }
 }
