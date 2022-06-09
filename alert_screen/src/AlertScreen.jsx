18d6c3caacb34446f1191d4d3d8b3898fa5c2726
mehdi "Arcanite" Oueslati <mehdi.oueslati@epita.fr>
2022-06-10 00:46:34 +0200

    website done! It currently listens to http://127.0.0.1:8080/alerts/

--- alert_screen/src/AlertScreen.jsx
+++ alert_screen/src/AlertScreen.jsx
@@ -3,19 +3,57 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Logo from './Logo';
 import Alert from '@mui/material/Alert';
+import Divider from '@mui/material/Divider';
+
+document.latestPersonID = 0; //yes this is hacky, idgaf
+
+function addPeople2List(list, newList){
+	let output = [...list];
+	newList.map((person) =>{
+		output.push({id: document.latestPersonID++, name: person.name, peacescore: person.peacescore})
+	});
+	console.log(output);
+	return output;
+}
 
 function removeID(list, id){
 	return list.filter((elem) => elem.id !== id);
 }
 
+function fetchNewAlerts(callback){
+	fetch("/alerts/")
+	  .then(res =>{ 
+	    return res.json();
+	  })
+	  .then(data => {
+	  	console.log("received: ");
+	  	console.log(data);
+	    callback(data);
+	  })
+}
+
 function AlertScreen(props) {
-	const people = [
-		{id: 0, name: "Donna McCrohan", peacescore: "-5"},
-		{id: 1, name: "Rainer Maria Rilke", peacescore: "-47"},
-		{id: 2, name: "Louis Nucera", peacescore: "0"},
-		{id: 3, name: "William S. Burroughs", peacescore: "-84"},
-	]
-	const [peopleList, setPeopleList] = useState(people);
+	const [peopleList, setPeopleList] = useState([]);
+	const [refresher, setRefresher] = useState(false);
+
+	const getNewAlerts = () => {
+		fetchNewAlerts(
+			(newPeople) => setPeopleList(addPeople2List(peopleList, newPeople))
+		);
+	};
+
+	useEffect(() => {
+		const fetchAndWait = async () => {
+			function sleep(ms) {
+			  return new Promise(resolve => setTimeout(resolve, ms));
+			}
+			getNewAlerts();
+			await sleep(1000);
+			setRefresher(!refresher);
+		};
+		fetchAndWait();
+	}, [refresher]);
+
 	return (
 		<Grid container style={{
 			background: "linear-gradient(180deg, rgba(9,9,121,1) 0%, rgba(2,0,36,1) 25%)",
@@ -26,14 +64,17 @@ function AlertScreen(props) {
 		  <Grid item lg={12}>
 			<Logo width="20vw" color="#FFF"/>
 		  </Grid>
-		  <Grid item lg={12}>
+		  <Grid item lg={12} style={{minHeight: "15vh"}}>
 			  <Typography variant="h2"> PeaceCorp<span style={{color: "yellow"}}>™</span> - The Eye </Typography>
+		  </Grid>
+		  <Grid item lg={12}>
+		  	<Divider />
 		  </Grid>
 		  <Grid item lg={2}/>
 		  <Grid item lg={8}>
 		  	<Grid container direction={'column'} spacing={2}>
 		  		{peopleList.map((person) => (
-  			  		<Grid item lg={12}>
+  			  		<Grid item lg={12} key={person.id}>
   					  	<Alert severity="warning" onClose={() => {setPeopleList(removeID(peopleList, person.id))}}>
   					  		<Typography variant="subtitle1" align="center">
   					  			<strong>[Peaceless individual detected]</strong>  -- The citizen <strong>{person.name}</strong> has an worrying peacescore of <strong>{person.peacescore}.</strong>. 
