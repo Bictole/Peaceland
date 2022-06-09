928f3dd8352043ef67a4e1fcd4dd96d9b1c8a7cb
mehdi "Arcanite" Oueslati <mehdi.oueslati@epita.fr>
2022-06-09 13:49:58 +0200

    cleaned & commented

--- save_aggregate/src/main/scala/Main.scala
+++ save_aggregate/src/main/scala/Main.scala
@@ -23,130 +23,71 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Encoders
 import org.apache.spark.sql.SQLContext  
 
-//import scala.collection.JavaConversions._
 import scala.reflect.ClassTag
 
 object Main extends App {
-  def experiment() = {
-    val accessKey = "AKIAQTIILA2TLU7Y64XW"
-    val secretKey = "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"
-    
-    val sparkConf = new SparkConf()
-        .setMaster("local[*]")
-        .setAppName("save_aggregate")
-        .set("spark.driver.host", "127.0.0.1")
-
-    val streamContext = new StreamingContext(sparkConf, Seconds(15))
-    val sparkContext = streamContext.sparkContext
-    val spark: SparkSession = SparkSession.builder.config(sparkContext.getConf).getOrCreate()
-    val sqlContext = new org.apache.spark.sql.SQLContext(sparkContext)
-    //import sqlContext.implicits._
-
-    spark.sparkContext.setLogLevel("ERROR")
-
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", accessKey)
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", secretKey)
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
-
-    val kafkaParams = Map(
-        "bootstrap.servers" -> "localhost:9092",
-        "key.deserializer" -> classOf[StringDeserializer],
-        "value.deserializer" -> classOf[StringDeserializer],
-        "group.id" -> "alert"
-    )
-    
-    val topics = Array("peaceland")
-
-    val stream = KafkaUtils.createDirectStream[String, String](
-        streamContext,
-        PreferConsistent,
-        Subscribe[String,String](topics, kafkaParams)
-    )
-
-    import spark.implicits._
-
-    val list = Seq(
-      SaveableEvent(0, "", Coords(1.2, 1.2), List("foo", "bar"), List(Person("lmao", 3.6))),
-      SaveableEvent(0, "", Coords(1.2, 1.2), List("foo", "bar"), List(Person("lmao", 3.6))),
-      SaveableEvent(0, "", Coords(1.2, 1.2), List("foo", "bar"), List(Person("lmao", 3.6))),
-      SaveableEvent(0, "", Coords(1.2, 1.2), List("foo", "bar"), List(Person("lmao", 3.6))),
-      SaveableEvent(0, "", Coords(1.2, 1.2), List("foo", "bar"), List(Person("lmao", 3.6)))
-    )
-
-    val df = list.toDF
-    df.show()
-
-
-    stream.flatMap(record => {
-        // Declare classes format to deserialize
-        implicit val personFormat = Json.format[Person]
-        implicit val coordsFormat = Json.format[Coords]
-        implicit val eventFormat = Json.format[Event]
-        val json = Json.parse(record.value())
-        eventFormat.reads(json).asOpt
-      })
-      .map(event => {
-        println(event)
-        val serializedTime = event.timestamp.format(DateTimeFormatter.ISO_DATE_TIME)
-        SaveableEvent(event.peacewatcherID, serializedTime, event.location, event.words, event.persons)
-      }).saveAsTextFiles("s3a://arcatest0/testingsaveastext", ".txt")
-      //.saveAsObjectFile("s3a://arcatest0/testingsaveasobj", ".obj")
-/*/*      .map(event => {
-        val values = event.productIterator.toSeq.toArray
-        val encoderSchema = Encoders.product[SaveableEvent].schemaq
-        import org.apache.spark.sql.Row
-        import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
-        val row: Row = new GenericRowWithSchema(values, encoderSchema)
-        row
-      })*/
-      .map(event => List.fill(1)(event))
-      .reduce((a, b) => a ++ b)
-      .map(event => {println(event); event})
-      .map(
-        eventRows => {
-          val encoderSchema = Encoders.product[SaveableEvent].schema
-          println(eventRows.getClass)
-          //val df = eventRows.toDF
-          //df.show()
-          val rdd = spark.sparkContext.parallelize(eventRows)
-          val df = sqlContext.createDataFrame(rdd)
-          df.show()
-          df
-/*          val encoderSchema = Encoders.product[SaveableEvent].schema
-          //val eventList = eventRows
-          //val df = rdd.toDF()
-/*          val df = spark.createDataFrame(
-            spark.sparkContext.parallelize(eventList),
-            encoderSchema
-          )*/
-          //val s = eventRows.toSeq
-          //val df = s.toDF
-          val rdd = sparkContext.parallelize(eventRows)
-          //val rowRdd = rdd.map(v => Row(v: _*))
-          val df = spark.createDataFrame(eventRows, encoderSchema)
-          df.show(1, false)
-          //val df = eventList.toDF // nullptr exception here
-          val path = "s3a://arcatest0/archive"
-          putOnS3(df, path)*/
-        }
-      )*/
-      //.print()
-
-    streamContext.start()
-    streamContext.awaitTermination()
-  }
-
-  def getFromS3(configuredSpark: SparkSession, inAddress: String) = {
-    configuredSpark.read
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .json(inAddress)
-  }
-
-  def putOnS3(df: DataFrame, outAddress: String) = {
-    println(outAddress)
-    df.write.mode(SaveMode.Overwrite).parquet(outAddress)
-  }
+  //access and secret key for AWS S3, found on IAM
+  val accessKey = ""
+  val secretKey = ""
   
-  experiment()
+  val sparkConf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("save_aggregate")
+      .set("spark.driver.host", "127.0.0.1")
+
+  //here Seconds(15) means that we fetch the batch every 15 secs, if we were to put it to 1 day, we'd aggregate daily
+  val streamContext = new StreamingContext(sparkConf, Seconds(15))
+  val sparkContext = streamContext.sparkContext
+  val spark: SparkSession = SparkSession.builder.config(sparkContext.getConf).getOrCreate()
+  val sqlContext = new org.apache.spark.sql.SQLContext(sparkContext)
+
+  //I'm keeping this here as a reminder of how glad I am that we're no longer using it:
+  //import sqlContext.implicits._
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", accessKey)
+  spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", secretKey)
+  spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
+
+  val kafkaParams = Map(
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "alert"
+  )
+  
+  val topics = Array("peaceland")
+
+  val stream = KafkaUtils.createDirectStream[String, String](
+      streamContext,
+      PreferConsistent,
+      Subscribe[String,String](topics, kafkaParams)
+  )
+
+  //path to file that we will read from and write to
+  val filePath = "s3a://arcatest0/charmander"
+  val fileExtension = ".obj"
+
+  //this is an example of how to read. it won't work cause I reached my max cap on S3.
+  val obj = sparkContext.objectFile("s3a://arcatest0/charmander-*." + fileExtension)
+  obj.foreach(println)
+
+  stream.flatMap(record => { //deserializes records into instances of Event case class
+      // Declare classes format to deserialize
+      implicit val personFormat = Json.format[Person]
+      implicit val coordsFormat = Json.format[Coords]
+      implicit val eventFormat = Json.format[Event]
+      val json = Json.parse(record.value())
+      eventFormat.reads(json).asOpt
+    })
+    .map(event => { //print the received event, and convert the Event cas class to a Saveable Event to avoid timestamp issues
+      println(event)
+      val serializedTime = event.timestamp.format(DateTimeFormatter.ISO_DATE_TIME)
+      SaveableEvent(event.peacewatcherID, serializedTime, event.location, event.words, event.persons)
+    })
+    .saveAsObjectFiles(filePath, fileExtension) //save the batch as an object file that will contain a map partition of all elements of the batch
+  
+  streamContext.start()
+  streamContext.awaitTermination()
 }
